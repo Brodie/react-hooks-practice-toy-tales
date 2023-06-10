ad4a0e9897f05b0a2d163cc07307bc3e26f02a59
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-10 15:12:09 -0700

    get and load complete

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
-  return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+function ToyContainer({ toys }) {
+  const toyCards = toys.map((toyObj) => {
+    return <ToyCard key={toyObj.id} toy={toyObj}></ToyCard>;
+  });
+
+  return <div id="toy-collection">{toyCards}</div>;
 }
 
 export default ToyContainer;
